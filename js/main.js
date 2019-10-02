8c8b0f9de82c4cc929699209b3b75bf77cdb1bd8
Vladislav <vladiusmaier@mail.ru>
2019-10-02 22:29:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $('#form__settings').click(function() {
     $('.setting').css('display', 'block');
+    $('.form-game').css('display', 'none');
   });
 
   $('.setting__out').click(function() {
     $('.setting').css('display', 'none');
+    $('.form-game').css('display', 'block');
   });
 });
