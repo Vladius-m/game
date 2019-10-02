175e5fa9293b4811ef8a03ac4324aa47b09a50e2
Vladislav <vladiusmaier@mail.ru>
2019-10-02 21:15:16 +0300

    добавление модального окна

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#form__settings').click(function() {
+    $('.setteng').css('display', 'block');
+  });
+
+  $('.setting__icon').click(function() {
+    $('.setteng').css('display', 'none');
+  });
+});
